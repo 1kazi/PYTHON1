614a83d1aacb7fd1927e57dda821fd8400af5e71
kazi shakerodin <shakerodink@gmail.com>
2022-10-24 22:55:08 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+print("hello world")
+age = 20
+print(age)
+age = 20
+age=30
+print(age)
+#taking input from users
+#input("what is your name:    ")
+#declaring variable called namek
+# name = input("what is your name:   ")
+# print("hello"  + name)  #here we do string concatination
+#data type convertions
+# birth_year = input("enter your birth year:    ")
+# age = 2022 - int(birth_year)
+# print(age)
+# calculate the sum of numbersc
+first = input("first:   ")
+second = input("second:   ")
+sum =int( first) +int(second)
+print(sum)
+first = input("first:   ")
+second = input("second:   ")
+sum =float( first) +float(second)
+print(sum)
